0cfd895359301ca91b6b2adca9607c07456bbae3
Anas Akhomach <anasakhomach205@gmail.com>
2025-01-06 18:10:48 +0100

    Create analysing-landing-page-tests.sql

---
+++ analysing-landing-page-tests.sql
@@ -0,0 +1,37 @@
+	-- so the fist time the '/lander-1' was viewed was at 2012-06-19 00:35:54, with this pageview_id: 23504
+    -- to compete this request we need to:
+    -- STEP 1: find the first website_pageview_id for the relevant sessions
+    
+SELECT 
+	website_pageview_id,
+	created_at,
+    pageview_url
+FROM website_pageviews
+WHERE created_at < '2012-06-28' AND pageview_url = '/lander-1'
+ORDER BY created_at ASC
+LIMIT 1;
+    -- STEP 2: identify the landing page of each session tha came after the session that have the the pageview_id = 23504
+    -- by definition the landing page is the first page visited in a session
+DROP TEMPORARY TABLE IF EXISTS first_pv;
+CREATE TEMPORARY TABLE first_pv
+SELECT
+website_sessions.website_session_id,
+MIN(website_pageviews.website_pageview_id) AS landing_page_id
+FROM website_pageviews
+	LEFT JOIN website_sessions
+		ON website_sessions.website_session_id = website_pageviews.website_session_id  
+        AND website_sessions.created_at < '2012-06-28'
+        AND website_pageviews.website_pageview_id > 23504
+        AND website_pageviews.pageview_url IN ('/home', '/lander-1')
+        AND website_sessions.utm_source = 'gsearch'
+        AND website_sessions.utm_campaign = 'nonbrand'
+WHERE website_sessions.website_session_id IS NOT NULL
+GROUP BY 1;
+SELECT * FROM first_pv; -- QA
+    -- STEP 3a: count the pageviews for each session.
+    -- STEP 3b: limit the count to the sessions that have only 1 pageview
+
+    -- the landing_page_id is basically a website_pageviw_id plus the condition (first page this session)
+
+
+    -- STEP 4: summerize! bounce_rate = count(bonced_sessions) / count(total_sessions)
