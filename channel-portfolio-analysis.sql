cd45a10d8e8b9f7aed514f3490815d8064b20293
Anas Akhomach <anasakhomach205@gmail.com>
2025-02-04 16:21:06 +0100

    Update channel-portfolio-analysis.sql

--- channel-portfolio-analysis.sql
+++ channel-portfolio-analysis.sql
@@ -4,6 +4,27 @@ select
     count(case when utm_source = 'gsearch' then website_session_id else null end) as gsearch_sessions,
     count(case when utm_source = 'bsearch' then website_session_id else null end) as bsearch_sessions
 from website_sessions
-where created_at between '2012-08-22' and '2012-11-29'
+where created_at > '2012-08-22' 
+	and created_at <'2012-11-29'
 	and utm_campaign = 'nonbrand'
-group by yearweek(created_at) 
+group by yearweek(created_at);
+
+select
+	utm_source,
+	count(website_session_id) as sessions,
+    count(case when device_type = 'mobile' then website_session_id else null end) as mobile_sessions,
+    count(case when device_type = 'mobile' then website_session_id else null end) / count(website_session_id) as pct_mobile
+from website_sessions
+where created_at > '2012-08-22'
+	and created_at < '2012-11-30'
+    and utm_campaign = 'nonbrand'
+group by 1;
+
+select
+*
+from website_sessions
+	left join orders
+		on website_sessions.website_session_id = orders.website_session_id
+where website_sessions.created_at > '2012-08-22'
+	and website_sessions.created_at < '2012-11-18'
+    and utm_campaign = 'nonbrand'
